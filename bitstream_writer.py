c31e91436945895d1086904b5c08da6ca9d3ef91
Noam <cohennoam48@gmail.com>
2023-04-28 10:55:02 +0300

    finished proxy object

--- bitstream_writer.py
+++ bitstream_writer.py
@@ -4,42 +4,57 @@ import bitstring
 
 
 class BitStreamWriter:
-    def __init__(self, stream: bitstring.BitStream) -> None:
+    def __init__(self) -> None:
         super().__init__()
-        self.stream = stream
+        self._stream = bitstring.BitStream()
+
+    def _insert(self, bits: bitstring.Bits) -> None:
+        self._stream.append(bits)
 
     def write_bytes(self, input_byte: bytes) -> None:
-        return self.stream.append(bitstring.Bits(bytes=input_byte))
+        bits = bitstring.Bits(bytes=input_byte)
+        self._insert(bits)
 
     def write_bool(self, input_bool: bool) -> None:
-        return self.stream.append(bitstring.Bits(bool=input_bool))
+        bits = bitstring.Bits(bool=input_bool)
+        self._insert(bits)
 
     def write_unsigned_integer(self, input_num: int, length: int, unit: Literal['bits', 'bytes']) -> None:
         """
         receive a number and unit (can be 'bits' or 'bytes') and reads n times in the unit size and returns the int
         representation. if in bytes, reads in little endian
         """
-
         if unit == "bits":
-            return self.stream.append(bitstring.Bits(int=input_num, length=length))
+            bits = bitstring.Bits(uint=input_num, length=length)
         elif unit == "bytes":
-            return self.stream.append(bitstring.Bits(int=input_num, length=8 * length))
+            bits = bitstring.Bits(uintle=input_num, length=8 * length)
         else:
             raise Exception("incorrect Unit passed, can be 'bits' or 'bytes'")
+        self._insert(bits)
 
-    def write_hex(self, input_hex: str, length: int, unit: Literal['bits', 'bytes']) -> None:
+    def write_hex(self, input_hex: str, bytes_length: int) -> None:
         """
 
         """
-        if unit == "bits":
-            return self.stream.append(bitstring.Bits(hex=input_hex, length=length))
-        elif unit == "bytes":
-            return self.stream.append(bitstring.Bits(hex=input_hex, length=8 * length))
-        else:
-            raise Exception("incorrect Unit passed, can be 'bits' or 'bytes'")
+        bits = bitstring.Bits(hex=input_hex, length=bytes_length)
+        self._insert(bits)
 
     def skip(self, n: int, unit: Literal['bits', 'bytes']):
         """
 
         """
-        return NotImplemented
+        raise NotImplemented
+
+    def __repr__(self):
+        return repr(self._stream)
+
+    def __str__(self):
+        return str(self._stream)
+
+
+if __name__ == '__main__':
+    stream = BitStreamWriter()
+    stream.write_bool(False)
+    stream.write_unsigned_integer(10, 2, 'bytes')
+    stream.write_hex('A3F3', 3)
+    print(stream._stream.bin)
