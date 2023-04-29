b955880a8b71c554c4f34cb045e066c6afa42241
Noam <cohennoam48@gmail.com>
2023-04-29 10:54:52 +0300

    changed utils.py to have better functions

--- utils.py
+++ utils.py
@@ -1,40 +1,27 @@
-def int_to_bits(num: int) -> str:
-    """
-    given int, returns the bits representation of the int as string without prefix
-    example: int_to_bits(11) = '1011'
-    """
-    # return bin(num).removeprefix('0b')
-    # Convert to binary string and remove prefix "0b"
-    binary_str = bin(num)[2:]
-
-    # Pad with zeros to ensure at least 8 bits
-    return '{0:0>8}'.format(binary_str)
+from itertools import zip_longest
+from typing import Literal
 
 
-def bits_to_int(bits: str, start: int = 0, size: int = 1) -> int:
+def grouper(iterable, n, *, incomplete=Literal['fill', 'strict', 'ignore'], fillvalue=None):
     """
-    given byte, returns the int representation of the bits from start and size amount
-    example: bits_to_int('1011', 2, 2) = 3
-        because, the bits from index 2 and 2 bits are '11' and is 3 in binary
+    Collect data into non-overlapping fixed-length chunks or blocks
+        grouper('ABCDEFG', 3, fillvalue='x') --> (A,B,C) (D,E,F) (G,x,x)
+        grouper('ABCDEFG', 3, incomplete='strict') --> (A,B,C) (D,E,F) ValueError
+        grouper('ABCDEFG', 3, incomplete='ignore') --> (A,B,C) (D,E,F)
     """
-    end: int = start + size
-    return int(bits[start:end], 2)
+    args = [iter(iterable)] * n
+    if incomplete == 'fill':
+        return zip_longest(*args, fillvalue=fillvalue)
+    if incomplete == 'strict':
+        return zip(*args, strict=True)
+    if incomplete == 'ignore':
+        return zip(*args)
+    else:
+        raise ValueError('Expected fill, strict, or ignore')
 
 
-def bytes_to_int(block: bytes, start: int = 0, size: int = 1) -> int:
+def chunker(size, seq):
     """
-    given bytes, returns the int representation of the bytes from start and size
-    example: bytes_to_int(b'\n\x00\n\x00\x91\x00\x00', 2, 2) = 10
-        because, the 2 bytes from index 2 are b'\n\x00\' which are 10 in little endian
+    chunker(3, "ABCDEFG") -> "ABC" "DEF" "G"
     """
-    end: int = start + size
-    return int.from_bytes(block[start:end], "little", signed=False)
-
-
-def single_byte(block: bytes, index: int) -> bytes:
-    """
-    given bytes array, returns a single byte at specific location
-    example: single_byte(b'\n\x00\n\x00\x91\x00\x00', 2) = b'\n'
-    """
-    end: int = index + 1
-    return block[index:end]
+    return (seq[pos:pos + size] for pos in range(0, len(seq), size))
